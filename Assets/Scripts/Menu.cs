37d5f5def694e053081bf8d2a6b155c969b79b12
Jeff <horusofoz@gmail.com>
2019-04-06 11:52:38 +1030

    Added menu screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    int WinSceneNumber;
+
+    public void LoadNextScene()
+    {
+        WinSceneNumber = SceneManager.sceneCountInBuildSettings - 1;
+
+        if (SceneManager.GetActiveScene().buildIndex < WinSceneNumber) // If not win scene
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0); // Start over
+        }
+    }
+}
